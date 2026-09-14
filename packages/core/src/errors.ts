dd318c59e9540466b856e44200bc3a082c6cd699
Mohamed F. Waziri <mohamedxiv@gmail.com>
2026-09-14 05:25:09 +0300

    feat: add authoring semantic errors

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -15,3 +15,30 @@ export class NativeBridgeError extends Error {
     this.name = 'NativeBridgeError';
   }
 }
+
+export class InvalidAuthoringRequestError extends Error {
+  readonly code = 'INVALID_AUTHORING_REQUEST' as const;
+
+  constructor(message = 'Invalid authoring request') {
+    super(message);
+    this.name = 'InvalidAuthoringRequestError';
+  }
+}
+
+export class PuppetAlreadyExistsError extends Error {
+  readonly code = 'PUPPET_ALREADY_EXISTS' as const;
+
+  constructor(message = 'Puppet output already exists') {
+    super(message);
+    this.name = 'PuppetAlreadyExistsError';
+  }
+}
+
+export class RoundTripMismatchError extends Error {
+  readonly code = 'ROUND_TRIP_MISMATCH' as const;
+
+  constructor(message = 'Puppet round-trip validation mismatch') {
+    super(message);
+    this.name = 'RoundTripMismatchError';
+  }
+}

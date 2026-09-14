3a4be347a531c8c8e8341e962d640dd567277715
Mohamed F. Waziri <mohamedxiv@gmail.com>
2026-09-14 05:25:16 +0300

    feat: export minimal authoring API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export function coreVersion(): string {
   return '0.1.0';
 }
 
+export * from './authoring.js';
 export * from './errors.js';
 export * from './inspection.js';
 export * from './native-host.js';

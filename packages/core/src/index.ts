f891f1a9592fe5f497dfdd84724e646863ba4e66
Mohamed F. Waziri <mohamedxiv@gmail.com>
2026-09-22 18:26:44 +0300

    feat: export physics capability discovery

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,6 +7,7 @@ export * from './errors.js';
 export * from './inspection.js';
 export * from './native-host.js';
 export * from './parameter-evaluation.js';
+export * from './physics-capabilities.js';
 export * from './preview.js';
 export * from './visual-authoring.js';
 export * from './visual-capabilities.js';
